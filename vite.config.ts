3b1fdbdc113b376bc162b47142cfd887caa0eea3
bashir Mosayyebi <119165074+Bashir1370@users.noreply.github.com>
2026-09-11 01:15:07 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'node:path'
+
+const appEntry = new URL(
+  './index.html',
+  import.meta.url,
+).pathname
+
+const editorEntry = new URL(
+  './editor.html',
+  import.meta.url,
+).pathname
 
 export default defineConfig({
   plugins: [react()],
+
   base: '/',
+
   build: {
     outDir: 'dist',
+
     rollupOptions: {
       input: {
-        app: resolve(__dirname, 'index.html'),
-        editor: resolve(__dirname, 'editor.html'),
+        app: appEntry,
+        editor: editorEntry,
       },
     },
   },
