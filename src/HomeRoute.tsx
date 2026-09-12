0fcb43fbd6519a5f4d0907f5b517691b3f3479e9
bashir Mosayyebi <119165074+Bashir1370@users.noreply.github.com>
2026-09-12 16:47:34 +0330

    Add Home account route wrapper

---
+++ src/HomeRoute.tsx
@@ -0,0 +1,6 @@
+import { DashboardApp } from './DashboardApp';
+import { HomeAccountPortal } from './HomeAccountPortal';
+
+export function HomeRoute() {
+  return <><DashboardApp/><HomeAccountPortal/></>;
+}
