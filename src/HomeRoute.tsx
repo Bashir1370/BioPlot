352ca41648147ad02ca21f005cbfe719c4fa81f2
bashir Mosayyebi <119165074+Bashir1370@users.noreply.github.com>
2026-09-14 17:27:03 +0330

    Remove recent projects section from default Home view

--- src/HomeRoute.tsx
+++ src/HomeRoute.tsx
@@ -1,7 +1,8 @@
 import { DashboardApp } from './DashboardApp';
 import { HomeAccountPortal } from './HomeAccountPortal';
+import { HomeProjectsMode } from './HomeProjectsMode';
 import { PortfolioShowcasePortal } from './PortfolioShowcase';
 
 export function HomeRoute() {
-  return <><DashboardApp/><PortfolioShowcasePortal/><HomeAccountPortal/></>;
+  return <><DashboardApp/><HomeProjectsMode/><PortfolioShowcasePortal/><HomeAccountPortal/></>;
 }
