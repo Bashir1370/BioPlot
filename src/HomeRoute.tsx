33fabd4faa210f7a81b54a7a776480bc33b334c9
bashir Mosayyebi <119165074+Bashir1370@users.noreply.github.com>
2026-09-14 21:15:52 +0330

    Move scientific visual hub out of Home

--- src/HomeRoute.tsx
+++ src/HomeRoute.tsx
@@ -1,9 +1,9 @@
 import { DashboardApp } from './DashboardApp';
 import { HomeAccountPortal } from './HomeAccountPortal';
 import { HomeProjectsMode } from './HomeProjectsMode';
+import { HomeTemplatesNavigation } from './HomeTemplatesNavigation';
 import { PortfolioShowcasePortal } from './PortfolioShowcase';
-import { ScientificVisualHubHomePortal } from './ScientificVisualHubHome';
 
 export function HomeRoute() {
-  return <><DashboardApp/><HomeProjectsMode/><ScientificVisualHubHomePortal/><PortfolioShowcasePortal/><HomeAccountPortal/></>;
+  return <><DashboardApp/><HomeProjectsMode/><HomeTemplatesNavigation/><PortfolioShowcasePortal/><HomeAccountPortal/></>;
 }
