c75561b1979f3157d0a908154a7ebf354dad3671
bashir Mosayyebi <119165074+Bashir1370@users.noreply.github.com>
2026-09-14 15:26:36 +0330

    Add editable project-backed squeeze portfolio showcase
    * Add project-backed squeeze portfolio showcase
    
    * Style squeeze portfolio showcase
    
    * Mount portfolio showcase in home workspace

--- src/HomeRoute.tsx
+++ src/HomeRoute.tsx
@@ -1,6 +1,7 @@
 import { DashboardApp } from './DashboardApp';
 import { HomeAccountPortal } from './HomeAccountPortal';
+import { PortfolioShowcasePortal } from './PortfolioShowcase';
 
 export function HomeRoute() {
-  return <><DashboardApp/><HomeAccountPortal/></>;
+  return <><DashboardApp/><PortfolioShowcasePortal/><HomeAccountPortal/></>;
 }
