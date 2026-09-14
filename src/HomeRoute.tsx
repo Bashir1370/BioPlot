45a3cda177f2692eb529cf8d38658535a500c1d2
bashir Mosayyebi <119165074+Bashir1370@users.noreply.github.com>
2026-09-14 20:53:06 +0330

    Mount Scientific Visual Hub on Home

--- src/HomeRoute.tsx
+++ src/HomeRoute.tsx
@@ -2,7 +2,8 @@ import { DashboardApp } from './DashboardApp';
 import { HomeAccountPortal } from './HomeAccountPortal';
 import { HomeProjectsMode } from './HomeProjectsMode';
 import { PortfolioShowcasePortal } from './PortfolioShowcase';
+import { ScientificVisualHubHomePortal } from './ScientificVisualHubHome';
 
 export function HomeRoute() {
-  return <><DashboardApp/><HomeProjectsMode/><PortfolioShowcasePortal/><HomeAccountPortal/></>;
+  return <><DashboardApp/><HomeProjectsMode/><ScientificVisualHubHomePortal/><PortfolioShowcasePortal/><HomeAccountPortal/></>;
 }
